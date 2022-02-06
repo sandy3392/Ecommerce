3e004f3f89efb9256c1eadbfeb288793d0c4f143
Sandeep Vittala <applesandy33@gmail.com>
2022-02-06 16:06:07 -0500

    adding category delete and put routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -34,14 +34,46 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+    .then(category => res.json(category))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, { 
+    where: {
+      id: req.params.id
+    }
+   })
+      .then(category => res.json(category))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(category => {
+      if (!category) {
+        res.status(404).json({ message: 'No category found with this id' });
+        return;
+      }
+      res.json(category);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
